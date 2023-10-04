fc46dab1b15cf5ffd4f3e378a8b9f171ec63cba8
98609 <98609@10.232.169.26>
2023-10-04 15:39:49 -0500

    flah

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -6,6 +6,25 @@ import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
+		StdDraw.setCanvasSize(300,200);
+		StdDraw.setXscale(0, 3); // Set the x-axis scale (0 to 3)
+        StdDraw.setYscale(0, 2);
+		StdDraw.setPenColor(Color.red);
+		StdDraw.filledRectangle(2.5,1,0.5,1);
+		StdDraw.setPenColor(Color.white);
+		StdDraw.filledRectangle(1.5,1,0.5,1);
+		StdDraw.setPenColor(Color.blue);
+		StdDraw.filledRectangle(0.5,1,0.5,1);
 		
+		/*StdDraw.setCanvasSize(800,600);
+		 StdDraw.setXscale(0, 1); // Set the x-axis scale (0 to 1)
+	        StdDraw.setYscale(0, 1); // Set the y-axis scale (0 to 1)
+
+	        // Define the vertices of the star
+	        double[] x = {0.5, 0.6, 0.65, 0.7, 0.8};
+	        double[] y = {0.7, 0.85, 0.65, 0.85, 0.7};
+
+	        // Draw the star
+	        StdDraw.polygon(x, y);*/
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -1,5 +1,6 @@
 package studio4;
 
+import java.awt.Color;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
@@ -20,5 +21,45 @@ public class InterpretDrawingFile {
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
 		
-	}
+		in.hasNext();
+			String shape = in.next();
+				
+		in.hasNextInt();
+		int int1= in.nextInt();
+		int int2= in.nextInt();	
+		int int3= in.nextInt();
+		
+		in.hasNextBoolean();
+			boolean a = in.nextBoolean();
+		
+		in.hasNextDouble();
+			double value1 = in.nextDouble();
+			double value2 = in.nextDouble();
+			double value3 = in.nextDouble();
+			double value4 = in.nextDouble();
+			double value5 = in.nextDouble();
+			double value6 = in.nextDouble();
+			
+		Color purply = new Color(int1, int2, int3);
+		StdDraw.setPenColor(purply);
+		
+		if (shape.equals("rectangle")) {
+			if (a==true)
+			StdDraw.filledRectangle(value1, value2, value3, value4);
+			if (a!=true)
+			StdDraw.rectangle(value1, value2, value3, value4);
+		}
+		if (shape.equals("ellipse")) {
+			if (a==true)
+			StdDraw.filledEllipse(value1, value2, value3, value4);
+			if (a!=true)
+			StdDraw.ellipse(value1, value2, value3, value4);
+		}
+		/*if (shape.equals("triangle")) {
+			if (a==true)
+			StdDraw.filledPolygon(value1, value2, value3, value4,value5,value6);
+			if (a!=true)
+			StdDraw.polygon(value1, value2, value3, value4,value5,value6);
+		}*/
+}
 }
